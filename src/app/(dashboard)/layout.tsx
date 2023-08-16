0567a7b66593d1269ba6df60f93d6725c6910cda
ZeberMVP <rubeen722@gmail.com>
2023-08-16 12:31:50 +0200

    feat: Sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,15 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className='h-full relative'>
-			<div className='hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900'>
-				Hello Sidebar
-			</div>
+			<Sidebar />
+
+			<main className='md:pl-72'>
+				<Navbar />
+				{children}
+			</main>
 		</div>
 	)
 }
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,28 @@
+'use client'
+
+import { useAuth } from '@clerk/nextjs'
+import Link from 'next/link'
+import { useEffect } from 'react'
+
 const page = () => {
-	return <div>Landing Page (Unprotected)</div>
+	const { isSignedIn } = useAuth()
+
+	useEffect(() => {
+		// Verificar si el usuario está signed in y redirigir a /dashboard si es así
+		if (isSignedIn) {
+			window.location.href = '/dashboard'
+		}
+	}, [isSignedIn])
+
+	return (
+		<div>
+			Landing Page (Unprotected)
+			<div>
+				{' '}
+				<Link href='/dashboard'>Sign In</Link>
+			</div>
+		</div>
+	)
 }
 
 export default page
